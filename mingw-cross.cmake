0342d96418a56bad9b23eb2091e99d15c2d0c0d8
Tobias Gruetzmacher <tobias-git@23.gs>
2012-06-28 09:26:58 +0200

    Some small changes for cygwin build.

--- mingw-cross.cmake
+++ mingw-cross.cmake
@@ -1,9 +1,10 @@
 # Run cmake like this:
 #   cmake -D CMAKE_TOOLCHAIN_FILE=../mingw-cross.cmake ..
 SET(CMAKE_SYSTEM_NAME Windows)
-SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
+SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres) # Debian
+#SET(CMAKE_RC_COMPILER i686-pc-mingw32-windres) # Cygwin
 include(CMakeForceCompiler)
 # Prefix detection only works with compiler id "GNU"
 # CMake will look for prefixed g++, cpp, ld, etc. automatically
-CMAKE_FORCE_C_COMPILER(i586-mingw32msvc-gcc GNU)
-
+CMAKE_FORCE_C_COMPILER(i586-mingw32msvc-gcc GNU) # Debian
+#CMAKE_FORCE_C_COMPILER(i686-pc-mingw32-gcc GNU) # Cygwin
